9b2abf601c004808f9cc39c8834479a32b418e2e
shuboyrawat <65123964+shuboyrawat@users.noreply.github.com>
2022-07-04 12:42:08 +0530

    first commit

--- src/pages/DatePage/Date.js
+++ src/pages/DatePage/Date.js
@@ -1,106 +1,72 @@
 import React, { useState } from 'react'
 import './Date.css'
 
-const Date = () => {
-  // let time = new Date()
-  const [fromdate, setfromDate] = useState('')
-  const [fromtime, setfromTime] = useState('')
-  const [todate, settoDate] = useState('')
-  const [totime, settoTime] = useState('')
+const Datedate = () => {
   const [duration, setduration] = useState('')
+  const [date1, setDate1] = useState(null)
+  const [date2, setDate2] = useState(null)
 
-  const getdate = () => {
-    var days = Math.floor(
-      (new Date(todate) - new Date(fromdate)) / 1000 / 60 / 60 / 24,
-    )
-    var hours = Math.floor(
-      ((new Date(todate) - new Date(fromdate)) / 1000 / 60 / 60) % 24,
-    )
-    var minutes = Math.floor(
-      ((new Date(todate) - new Date(fromdate)) / 1000 / 60) % 60,
-    )
+  // let date1 = new Date('06/30/2022')
+  // let date2 = new Date('07/30/2022')
+  // let Difference_In_Time = date1.getTime() - date2.getTime()
+  // let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24)
+  // console.log(Difference_In_Days)
+  // setduration(Difference_In_Days)
+
+  function gettime(duration) {
+    var days = Math.floor(duration / 1000 / 60 / 60 / 24),
+      hours = Math.floor((duration / 1000 / 60 / 60) % 24),
+      minutes = Math.floor((duration / 1000 / 60) % 60)
 
     let result = days + ' days ' + hours + ' hours ' + minutes + ' minutes '
-
+    // console.log(result)
     setduration(result)
   }
 
-  // function gettime(duration) {
-  //   var days = Math.floor(duration / 1000 / 60 / 60 / 24)
-  //   var hours = Math.floor((duration / 1000 / 60 / 60) % 24)
-  //   var minutes = Math.floor((duration / 1000 / 60) % 60)
-
-  //   let result = days + ' days ' + hours + ' hours ' + minutes + ' minutes '
-  //   console.log(result)
-  //   let a = 34
-  //   setduration(a)
-  // }
-
   return (
     <div className="dateandtime">
-      <div className="pickerbody">
-        <div className="dur">
-          <label>Duration</label>
+      <div className="dateandtimediv">
+        <div className="datetimepicker">
+          <div className="cont">
+            <p>From Date:</p>
+            <div className="date">
+              <input
+                value={date1}
+                type="datetime-local"
+                onChange={(e) => setDate1(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="cont">
+            <p>To Date:</p>
+            <div className="date">
+              <input
+                value={date2}
+                type="datetime-local"
+                onChange={(e) => setDate2(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="btn">
+          <button onClick={() => gettime(new Date(date2) - new Date(date1))}>
+            Get Duration
+          </button>
+        </div>
+        <div className="duration">
+          <label htmlFor="">Duration:</label>
           <input
             type="text"
             name="duration"
-            placeholder="Duration"
-            onChange={(e) => setduration(e.target.value)}
             value={duration}
+            onChange={(e) => setduration(e.target.value)}
           />
         </div>
-
-        <div className="pickers">
-          <div className="cont">
-            <span>From Date</span>
-            <div className="date">
-              <p>Date picker</p>
-              <input
-                type="date"
-                value={fromdate}
-                onChange={(e) => setfromDate(e.target.value)}
-              />
-            </div>
-
-            <div className="time">
-              <div>
-                <p>Time picker</p>
-                <input
-                  type="time"
-                  value={fromtime}
-                  onChange={(e) => setfromTime(e.target.value)}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="cont">
-            <span>To Date</span>
-            <div className="date">
-              <p>Date picker</p>
-              <input
-                type="date"
-                value={todate}
-                onChange={(e) => settoDate(e.target.value)}
-              />
-            </div>
-            <div className="time">
-              <div>
-                <p>Time picker</p>
-                <input
-                  type="time"
-                  value={totime}
-                  onChange={(e) => settoTime(e.target.value)}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <button onClick={getdate}>Click</button>
       </div>
     </div>
   )
 }
 
-export default Date
+export default Datedate
